39e04fb225ad92c946639673401620d3f278b9c7
ajfedorec <ajfedorec@users.noreply.github.com>
2022-04-28 13:37:54 +0100

    added Maria's data with perturbations

--- gMLV/gMLV_ML.py
+++ gMLV/gMLV_ML.py
@@ -21,13 +21,13 @@ from sklearn.base import RegressorMixin
 class Ridge1(BaseEstimator, RegressorMixin):
     """Custom ridge regression class"""
 
-    def __init__(self, alphas=[0.1, 0.1], nsp=3):
+    def __init__(self, alphas=[0.1, 0.1], num_species=3):
         self.alphas = alphas
-        self.nsp = nsp
+        self.num_species = num_species
 
     def fit(self, X, y):
         # print("calling fit")
-        self.coef_ = ridge_fit(X.T, y.T, self.alphas, self.nsp)
+        self.coef_ = ridge_fit(X.T, y.T, self.alphas, self.num_species)
         # return self
 
     def predict(self, X):
@@ -35,7 +35,7 @@ class Ridge1(BaseEstimator, RegressorMixin):
 
     def get_params(self, deep=True):
         # suppose this estimator has parameters "alpha" and "recursive"
-        return {"alphas": self.alphas, "nsp": self.nsp}
+        return {"alphas": self.alphas, "num_species": self.num_species}
 
     # def set_params(self, **parameters):
     #    for parameter, value in parameters.items():
@@ -46,13 +46,14 @@ class Ridge1(BaseEstimator, RegressorMixin):
 class Ridge2(BaseEstimator, RegressorMixin):
     """Custom ridge regression class"""
 
-    def __init__(self, alphas=[0.1, 0.1, 0.1], nsp=3):
+    def __init__(self, alphas=[0.1, 0.1, 0.1], num_species=3, num_pert=1):
         self.alphas = alphas
-        self.nsp = nsp
+        self.num_species = num_species
+        self.num_pert = num_pert
 
     def fit(self, X, y):
         # print("calling fit")
-        self.coef_ = ridge_fit_pert(X.T, y.T, self.alphas, self.nsp)
+        self.coef_ = ridge_fit_pert(X.T, y.T, self.alphas, self.num_species, self.num_pert)
         # return self
 
     def predict(self, X):
@@ -60,35 +61,36 @@ class Ridge2(BaseEstimator, RegressorMixin):
 
     def get_params(self, deep=True):
         # suppose this estimator has parameters "alpha" and "recursive"
-        return {"alphas": self.alphas, "nsp": self.nsp}
+        return {"alphas": self.alphas, "num_species": self.num_species, "num_pert": self.num_pert}
 
 
-def ridge_fit(X, F, alphas, nsp):
-    # To do: redo this with transposed X and Y
-    
+def ridge_fit(X, F, alphas, num_species):
+    # To do: redo this with transpose X and Y
+
     # standard least squares
     # beta = np.dot( np.dot(F,np.transpose(X)), la.inv(np.dot(X,np.transpose(X))))
-    
+
     # compute ridge estimate
-    penalty = np.diagflat(np.hstack([np.repeat(alphas[0], nsp), alphas[1]]))
-    
-    beta = (F @ X.T) @ la.inv(X @ X.T + penalty) 
-    
+    penalty = np.diagflat(np.hstack([np.repeat(alphas[0], num_species), alphas[1]]))
+
+    beta = (F @ X.T) @ la.inv(X @ X.T + penalty)
+
     return beta
 
 
-def ridge_fit_pert(X, F, alphas, nsp):
+def ridge_fit_pert(X, F, alphas, num_species, num_pert):
     # To do: redo this with transposed X and Y
-    
+
     # standard least squares
     # beta = np.dot( np.dot(F,np.transpose(X)), la.inv(np.dot(X,np.transpose(X))))
-    
+
     # compute ridge estimate
-    penalty = np.diagflat(np.hstack([np.repeat(alphas[0], nsp), alphas[1], alphas[2]]))
-    
-    beta = (F @ X.T) @ la.inv(X @ X.T + penalty) 
-    
+    penalty = np.diagflat(np.hstack([np.repeat(alphas[0], num_species), alphas[1], np.repeat(alphas[2], num_pert)]))
+
+    beta = np.dot(F, X.T) @ la.inv(X @ X.T + penalty)
+
     return beta
+
 
 # can use
 # import importlib
@@ -105,7 +107,7 @@ def ridge_fit_test(X, Y):
     print(model.predict(tX))
 
     print("custom ridge")
-    model = cRidge(alphas=[0.01, 0.01], nsp=nsp)
+    model = cRidge(alphas=[0.01, 0.01], num_species=num_species)
     model.fit(tX, tF)
     print(model.coef_)
     print(model.coef_.shape)
@@ -113,12 +115,12 @@ def ridge_fit_test(X, Y):
 
 
 def linearize_time_course_16S(yobs, times):
-    nsp = yobs.shape[1]
+    num_species = yobs.shape[1]
     nt = len(times)
 
     # F = dlnX/dt
     DlnX = np.diff(np.log(yobs), axis=0)
-    Dt = np.tile(np.diff(times), (nsp, 1))
+    Dt = np.tile(np.diff(times), (num_species, 1))
     # print(DlnX)
     # print(Dt)
     F = np.divide(DlnX, np.transpose(Dt))
@@ -144,12 +146,12 @@ def linearize_time_course_16S(yobs, times):
 
 # here u should be of length timepoints
 def linearize_time_course_16S_u(yobs, times, u):
-    nsp = yobs.shape[1]
+    num_species = yobs.shape[1]
     nt = len(times)
 
     # F = dlnX/dt
     DlnX = np.diff(np.log(yobs), axis=0)
-    Dt = np.tile(np.diff(times), (nsp, 1))
+    Dt = np.tile(np.diff(times), (num_species, 1))
     F = np.divide(DlnX, np.transpose(Dt))
 
     # X matrix: stacked observed counts
@@ -165,7 +167,7 @@ def linearize_time_course_16S_u(yobs, times, u):
 def linearise_time_course_metabolites(sobs, yobs, times):
     nm = sobs.shape[1]
     ns = yobs.shape[1]
-    
+
     # S = ds/dt
     DS = np.diff(sobs, axis=0)
     Dt = np.tile(np.diff(times), (nm, 1))
@@ -206,11 +208,11 @@ def plot_coeffs():
     plt.show()
 
 
-def fit_alpha_Ridge1(X, F, nsp, n_a0, n_a1):
+def fit_alpha_Ridge1(X, F, num_species, n_a0, n_a1):
     # use own ridge model
-    
-    a0 = np.logspace(-2, 2, n_a0)    # constraint on Mij matrix elements
-    a1 = np.logspace(-6, 0, n_a1)    # constraint on mu
+
+    a0 = np.logspace(-2, 2, n_a0)  # constraint on Mij matrix elements
+    a1 = np.logspace(-6, 0, n_a1)  # constraint on mu
 
     xv, yv = np.meshgrid(a0, a1, indexing='ij')
 
@@ -218,8 +220,8 @@ def fit_alpha_Ridge1(X, F, nsp, n_a0, n_a1):
     for i in range(n_a0):
         for j in range(n_a1):
             # print(i, j, xv[i,j], yv[i,j])
-            candidate_regressors.append(Ridge1(alphas=[xv[i, j], yv[i, j]], nsp=nsp))
-    
+            candidate_regressors.append(Ridge1(alphas=[xv[i, j], yv[i, j]], num_species=num_species))
+
     cv = RepeatedKFold(n_splits=10, n_repeats=10)
     cv_results = [-cross_val_score(r, X, F, scoring='neg_root_mean_squared_error', cv=cv) for r in candidate_regressors]
 
@@ -231,20 +233,20 @@ def fit_alpha_Ridge1(X, F, nsp, n_a0, n_a1):
     print("minimum found: a0/a1/error:", a0[inds[0]], a1[inds[1]], cv_means[min_i])
 
     # unconstrained to compare
-    unc_model = Ridge1(alphas=[0, 0], nsp=nsp)
-    cv_results = -cross_val_score(unc_model, X, F, scoring='neg_root_mean_squared_error', cv=cv) 
+    unc_model = Ridge1(alphas=[0, 0], num_species=num_species)
+    cv_results = -cross_val_score(unc_model, X, F, scoring='neg_root_mean_squared_error', cv=cv)
     print("unconstrained error        :", np.mean(cv_results))
-    
+
     return a0[inds[0]], a1[inds[1]]
 
 
-def fit_alpha_Ridge2(X, F, nsp, n_a0, n_a1, n_a2):
+def fit_alpha_Ridge2(X, F, num_species, num_pert, n_a0, n_a1, n_a2):
     # use own ridge model
-    
-    a0 = np.logspace(-2, 2, n_a0)    # constraint on Mij matrix elements
-    a1 = np.logspace(-6, 0, n_a1)    # constraint on mu
-    a2 = np.logspace(-6, 0, n_a2)    # constraint on epsilon
-    
+
+    a0 = np.logspace(-2, 2, n_a0)  # constraint on Mij matrix elements
+    a1 = np.logspace(-6, 0, n_a1)  # constraint on mu
+    a2 = np.logspace(-6, 0, n_a2)  # constraint on epsilon
+
     xv, yv, zv = np.meshgrid(a0, a1, a2, indexing='ij')
 
     candidate_regressors = []
@@ -252,8 +254,10 @@ def fit_alpha_Ridge2(X, F, nsp, n_a0, n_a1, n_a2):
         for j in range(n_a1):
             for k in range(n_a2):
                 # print(i, j, xv[i,j], yv[i,j])
-                candidate_regressors.append(Ridge2(alphas=[ xv[i, j, k], yv[i, j, k], zv[i, j, k] ], nsp=nsp))
-    
+                candidate_regressors.append(Ridge2(alphas=[xv[i, j, k], yv[i, j, k], zv[i, j, k]],
+                                                   num_species=num_species,
+                                                   num_pert=num_pert))
+
     cv = RepeatedKFold(n_splits=10, n_repeats=10)
     cv_results = [-cross_val_score(r, X, F, scoring='neg_root_mean_squared_error', cv=cv) for r in candidate_regressors]
 
@@ -265,69 +269,71 @@ def fit_alpha_Ridge2(X, F, nsp, n_a0, n_a1, n_a2):
     print("minimum found: a0/a1/a2/error:", a0[inds[0]], a1[inds[1]], a2[inds[2]], cv_means[min_i])
 
     # unconstrained to compare
-    unc_model = Ridge2(alphas=[0, 0, 0], nsp=nsp)
-    cv_results = -cross_val_score(unc_model, X, F, scoring='neg_root_mean_squared_error', cv=cv) 
+    unc_model = Ridge2(alphas=[0, 0, 0], num_species=num_species, num_pert=num_pert)
+    cv_results = -cross_val_score(unc_model, X, F, scoring='neg_root_mean_squared_error', cv=cv)
     print("unconstrained error        :", np.mean(cv_results))
-    
+
     return a0[inds[0]], a1[inds[1]], a2[inds[2]]
 
 
-def do_final_fit_Ridge1(X, F, nsp, a0, a1):
-    model = Ridge1(alphas=[a0, a1], nsp=nsp)
+def do_final_fit_Ridge1(X, F, num_species, a0, a1):
+    model = Ridge1(alphas=[a0, a1], num_species=num_species)
     model.fit(X, F)
-    mu_h = [model.coef_[i][-1] for i in range(0, nsp)]
-    M_h = [model.coef_[i][0:nsp].tolist() for i in range(0, nsp)]
+    mu_h = [model.coef_[i][-1] for i in range(0, num_species)]
+    M_h = [model.coef_[i][0:num_species].tolist() for i in range(0, num_species)]
     return mu_h, M_h
 
 
-def do_final_fit_Ridge2(X, F, nsp, a0, a1, a2):
-    model = Ridge2(alphas=[a0, a1, a2], nsp=nsp)
+def do_final_fit_Ridge2(X, F, num_species, num_pert, a0, a1, a2):
+    model = Ridge2(alphas=[a0, a1, a2], num_species=num_species, num_pert=num_pert)
     model.fit(X, F)
-    e_h = [model.coef_[i][-1] for i in range(0, nsp)]
-    mu_h = [model.coef_[i][-2] for i in range(0, nsp)]
-    M_h = [model.coef_[i][0:nsp].tolist() for i in range(0, nsp)]
+
+    M_h = [model.coef_[i][0:num_species].tolist() for i in range(0, num_species)]
+    mu_h = [model.coef_[i][num_species] for i in range(0, num_species)]
+    e_h = [model.coef_[i][(num_species+1):-1] for i in range(0, num_species)]
+
     return mu_h, M_h, e_h
 
 
-def do_bootstrapping(X, F, nsp, a0, a1, nt, nboots=100):
+def do_bootstrapping(X, F, num_species, a0, a1, nt, nboots=100):
     # do some bootstrapping 
-    model = Ridge1(alphas=[a0, a1], nsp=nsp)
+    model = Ridge1(alphas=[a0, a1], num_species=num_species)
 
-    mus = np.zeros([nboots, nsp])
-    mms = np.zeros([nboots, nsp*nsp])
+    mus = np.zeros([nboots, num_species])
+    mms = np.zeros([nboots, num_species * num_species])
     for i in range(0, nboots):
-        sample_index = np.random.choice(range(0, nt-1), nt-1)
+        sample_index = np.random.choice(range(0, nt - 1), nt - 1)
 
         X_s = X[sample_index, :]
         F_s = F[sample_index, :]
-    
+
         model.fit(X_s, F_s)
-        mu_h = [model.coef_[i][-1] for i in range(0, nsp)]
-        M_h = [model.coef_[i][0:nsp].tolist() for i in range(0, nsp)]
+        mu_h = [model.coef_[i][-1] for i in range(0, num_species)]
+        M_h = [model.coef_[i][0:num_species].tolist() for i in range(0, num_species)]
 
         mus[i, :] = mu_h
         mms[i, :] = np.array(M_h).flatten()
-    
+
         # print(np.array(mu_h))
         # print(np.round(np.array(M_h),decimals=2))
 
     print("examining mu_i")
     mus_max = mus.max(axis=0)
     mus_min = mus.min(axis=0)
-    for i in range(0, nsp):
+    for i in range(0, num_species):
         star = ""
         if np.abs(mus_min[i] - mus_max[i]) > 1e-4:
             if mus_min[i] > 0 and mus_max[i] > 0:
                 star = "*"
             elif mus_min[i] < 0 and mus_max[i] < 0:
                 star = "*"
-        
+
         print(i, np.round(mus_min[i], decimals=3), " - ", np.round(mus_max[i], decimals=3), star)
-     
+
     mms_max = mms.max(axis=0)
     mms_min = mms.min(axis=0)
     print("\nexamining Mij")
-    for i in range(0, nsp*nsp):
+    for i in range(0, num_species * num_species):
         star = ""
         if np.abs(mms_min[i] - mms_max[i]) > 1e-4:
             if mms_min[i] > 0 and mms_max[i] > 0:
@@ -335,15 +341,16 @@ def do_bootstrapping(X, F, nsp, a0, a1, nt, nboots=100):
             elif mms_min[i] < 0 and mms_max[i] < 0:
                 star = "*"
 
-        print(i+1, np.unravel_index(i, (nsp, nsp)), np.round(mms_min[i], decimals=3), " - ", np.round(mms_max[i], decimals=3), star)
+        print(i + 1, np.unravel_index(i, (num_species, num_species)), np.round(mms_min[i], decimals=3), " - ",
+              np.round(mms_max[i], decimals=3), star)
 
 
 def plot_alpha_lasso(X, S, n_a):
     candidate_alpha = np.logspace(-1, 2, n_a)
     candidate_regressors = [Lasso(alpha=a, fit_intercept=False, max_iter=10000, tol=1e-1) for a in candidate_alpha]
-    
+
     coefs = [r.fit(X, S).coef_.flatten() for r in candidate_regressors]
-    
+
     plt.figure()
     ax = plt.gca()
     ax.plot(candidate_alpha, coefs)
@@ -358,12 +365,12 @@ def plot_alpha_lasso(X, S, n_a):
 
 
 def fit_alpha_lasso(X, S, n_a):
-    
     candidate_alpha = np.logspace(-1, 2, n_a)
     candidate_regressors = [Lasso(alpha=a, fit_intercept=False, max_iter=10000, tol=1e-1) for a in candidate_alpha]
-    
+
     cv = RepeatedKFold(n_splits=10, n_repeats=10)
-    cv_results = [-cross_val_score(r, X, S, scoring='neg_root_mean_squared_error', cv=cv, n_jobs=-1) for r in candidate_regressors]
+    cv_results = [-cross_val_score(r, X, S, scoring='neg_root_mean_squared_error', cv=cv, n_jobs=-1) for r in
+                  candidate_regressors]
 
     n_est = np.array([len(x) for x in cv_results])
     cv_means = np.array([np.mean(x) for x in cv_results])
@@ -378,18 +385,20 @@ def fit_alpha_lasso(X, S, n_a):
 
     plt.figure()
     plt.plot(candidate_alpha, cv_means)
-    plt.fill_between(candidate_alpha, cv_means + 1*cv_se, cv_means - 1*cv_se, alpha=.1)
+    plt.fill_between(candidate_alpha, cv_means + 1 * cv_se, cv_means - 1 * cv_se, alpha=.1)
     plt.axhline(cutoff, linestyle='dotted', label='Best + One SE')
-    plt.scatter([candidate_alpha[one_se_rule_i]], [cv_means[one_se_rule_i]], marker='o', color='orange', label='One SE Rule')
+    plt.scatter([candidate_alpha[one_se_rule_i]], [cv_means[one_se_rule_i]], marker='o', color='orange',
+                label='One SE Rule')
     plt.scatter([candidate_alpha[min_i]], [cv_means[min_i]], marker='o', color='blue', label='Minimum rule')
     plt.legend()
     plt.xscale('log')
     plt.xlabel('Log alpha')
     plt.ylabel('MSE')
     plt.show()
-    
+
     return candidate_alpha[min_i], candidate_alpha[one_se_rule_i]
-    
+
+
 ###########################################################
 # older function using other more standard methods. Might come back to these at some point
 
@@ -409,7 +418,7 @@ def fit_alpha_default():
     search = GridSearchCV(model, grid, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1)
     # perform the search
     results = search.fit(tX, tF)
-    
+
     # summarize
     print('MAE: %.3f' % results.best_score_)
     print('Config: %s' % results.best_params_)
@@ -422,13 +431,13 @@ def fit_alpha_default():
     # model = ElasticNet(alpha=0.01, fit_intercept=False, max_iter=100000, l1_ratio=0.9, tol=1e-2)
 
     model.fit(tX, tF)
-    mu_h = [model.coef_[i][-1] for i in range(0, nsp)]
-    M_h = [model.coef_[i][0:nsp].tolist() for i in range(0, nsp)]
+    mu_h = [model.coef_[i][-1] for i in range(0, num_species)]
+    M_h = [model.coef_[i][0:num_species].tolist() for i in range(0, num_species)]
 
     modelB = LinearRegression(fit_intercept=False)
     modelB.fit(tX, tF)
-    mu_l = [modelB.coef_[i][-1] for i in range(0, nsp)]
-    M_l = [modelB.coef_[i][0:nsp].tolist() for i in range(0, nsp)]
+    mu_l = [modelB.coef_[i][-1] for i in range(0, num_species)]
+    M_l = [modelB.coef_[i][0:num_species].tolist() for i in range(0, num_species)]
 
     print("\ninferred params:")
     print("mu_hat/mu/mu_l:")
@@ -441,7 +450,7 @@ def fit_alpha_default():
     print("\n", np.round(np.array(M_l), decimals=2))
 
     # plot the fit
-    yobs_pred = odeint(gLV, y0, times, args=(nsp, mu_h, M_h))
+    yobs_pred = odeint(gLV, y0, times, args=(num_species, mu_h, M_h))
     plt.plot(times, yobs)
     plt.plot(times, yobs_pred, '--')
 
@@ -451,5 +460,5 @@ def fit_alpha_default():
     plt.stem(np.arange(0, len(mu), dtype="int32"), np.array(mu), markerfmt="X")
 
     plt.figure()
-    plt.stem(np.arange(0, nsp*nsp), np.array(M_h).flatten(), markerfmt="D")
-    plt.stem(np.arange(0, nsp*nsp), np.array(M).flatten(), markerfmt="X")
+    plt.stem(np.arange(0, num_species * num_species), np.array(M_h).flatten(), markerfmt="D")
+    plt.stem(np.arange(0, num_species * num_species), np.array(M).flatten(), markerfmt="X")
