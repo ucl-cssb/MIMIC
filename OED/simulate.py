959fa41fc6df58b4cdc51bca1c6c81369680ebdc
Neythen <zcqsntr@ucl.ac.uk>
2022-05-18 17:16:15 +0100

    MPC OED working

--- OED/simulate.py
+++ OED/simulate.py
@@ -1,11 +1,10 @@
 import sys
 import os
 
-IMPORT_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'imports')
 
-sys.path.append(IMPORT_PATH)
-sys.path.append('/Users/neythen/Desktop/Projects/ROCC/')
-sys.path.append('/Users/neythen/Desktop/Projects/RED_master/')
+
+
+sys.path.append('../../RED_master/')
 
 import math
 from casadi import *
@@ -39,77 +38,27 @@ plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
-
-if __name__ == '__main__':
-    #sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-    eta = 0.01 # small constant to add to each FIM element ot prevent singularity
-
-
-    scale_factor = 0.1
-    M = np.ones((3,3)) * -scale_factor
-    gr = np.ones((3,1)) * scale_factor
-    E = np.eye(3) * -scale_factor # 3 antibiotics, one for each bacteria
-
-
-    # M = np.array([
-    #     [-0.1, 0, 0],
-    #     [0, -0.1, 0],
-    #     [0, 0, -0.1]
-    # ])
-
-    gr = np.array([
-        [1],
-        [11],
-        [12]
-    ])*scale_factor
-
-    y0 = [0.1,0.1,0.1]
-
-    params = np.hstack((M.flatten(), gr.flatten(), E.flatten())) # need to flatten for FIM calc
-    print(params)
-
-
-    params = DM(params)
-
-    print(params.size())
-    actual_params = params
-    N_control_intervals = 10
-    control_interval_time = 1 # AU
-    num_inputs = -1
-    input_bounds = [[0,1], [0,1], [0,1]]
-    n_observed_variables = 3
-    n_controlled_inputs = 3
-    dt = 1
-    normaliser = -1
-
-
-    save_path = './'
-
-
-    args = y0, xdot, params, actual_params, n_observed_variables, n_controlled_inputs, num_inputs, input_bounds, dt, control_interval_time,normaliser
-    env = OED_env(*args)
-
-
-
-
+def simulate(us, plot = False, calculate_FIM = True):
     # test simulation
-    us = np.random.rand(3, N_control_intervals)*scale_factor
-    #us = np.zeros((3,N_control_intervals))*scale_factor
-    #us = np.array([[0,0,1]]*N_control_intervals).T*scale_factor
 
-    calculate_FIM = True
+
+
 
     if calculate_FIM:
         env.CI_solver = env.get_control_interval_solver(control_interval_time, dt, mode='OED')
         trajectory_solver = env.get_sampled_trajectory_solver(N_control_intervals, control_interval_time, dt)
         est_trajectory = trajectory_solver(env.initial_Y, params,
-                                       reshape(us, (n_controlled_inputs, N_control_intervals)))
+                                           reshape(us, (n_controlled_inputs, N_control_intervals)))
+
+        est_trajectory = np.hstack((np.array(env.initial_Y).reshape(-1, 1), est_trajectory))
 
         print(est_trajectory)
         print(est_trajectory.shape)
         FIM = env.get_FIM(est_trajectory)
         FIM += DM(np.ones(FIM.size()) * eta)
         print(FIM)
+        print('trace FIM:', trace(FIM))
+        print('eigen values: ', np.linalg.eig(FIM)[0], np.sum(np.linalg.eig(FIM)[0]))
         print(FIM.size())
 
         q, r = qr(FIM)
@@ -120,29 +69,87 @@ if __name__ == '__main__':
             0]  # do it like this to protect from numerical errors from multiplying large EVs
         print('det FIM:', det_FIM)
         print('log det FIM:', logdet_FIM)
+
     else:
         env.CI_solver = env.get_control_interval_solver(control_interval_time, dt, mode='sim')
         trajectory_solver = env.get_sampled_trajectory_solver(N_control_intervals, control_interval_time, dt)
         est_trajectory = trajectory_solver(y0, params,
                                            reshape(us, (n_controlled_inputs, N_control_intervals)))
 
+        est_trajectory = np.hstack((np.array(y0).reshape(3, 1), est_trajectory))
 
-        est_trajectory = np.hstack((np.array(y0).reshape(3,1), est_trajectory))
-
-
-        plt.plot(us.T, alpha = 0.5, linewidth = 4)
+    if plot:
+        plt.plot(us.T, alpha=0.5, linewidth=4)
         plt.xlabel('Time (AU)')
         plt.ylabel('Perturbation (AU)')
-        plt.savefig('./working_dir/perturbation.png', dpi = 300)
+        plt.savefig('./working_dir/perturbation.png', dpi=300)
         plt.figure()
 
-        plt.plot(est_trajectory.T, alpha = 0.5, linewidth = 4)
+        plt.plot(est_trajectory[0:n_observed_variables,:].T, alpha=0.5, linewidth=4)
         plt.xlabel('Time (AU)')
         plt.ylabel('Population (AU)')
         plt.savefig('./working_dir/population.png', dpi=300)
 
+        plt.figure()
+        plt.spy(FIM)
+        plt.xticks(range(21),range(0,21))
+
+
         plt.show()
 
+
+if __name__ == '__main__':
+    #sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+    y0 = np.array([1.,1.,1.])
+
+    gMLV_params = json.load(open('gMLV_params.json'))
+    M = np.array(gMLV_params['M'])
+    E = np.array(gMLV_params['E'])
+    gr = np.array(gMLV_params['gr'])
+    y0 = np.array(gMLV_params['y0'])
+    scale_factor = gMLV_params['scale_factor']
+    eta = gMLV_params['eta']
+
+
+
+    M *= scale_factor
+    E *= scale_factor
+    gr*= scale_factor
+    y0 *= scale_factor
+
+    params = np.hstack((M.flatten(), gr.flatten(), E.flatten())) # need to flatten for FIM calc
+    print(params)
+
+
+
+
+    params = DM(params)
+
+    print(params.size())
+    actual_params = params
+    N_control_intervals = 10
+    control_interval_time = 10 # AU
+    num_inputs = -1
+    input_bounds = [[0,1], [0,1], [0,1]]
+    n_observed_variables = 3
+    n_controlled_inputs = 3
+    dt = 0.01
+    normaliser = -1
+
+
+    save_path = './'
+
+
+    args = y0, xdot, params, actual_params, n_observed_variables, n_controlled_inputs, num_inputs, input_bounds, dt, control_interval_time,normaliser
+    env = OED_env(*args)
+
+    us = np.random.rand(3, N_control_intervals)
+    #us = np.zeros((3,N_control_intervals))*scale_factor
+    #us = np.array([[0,0,1]]*N_control_intervals).T*scale_factor
+
+    us = np.load('working_dir/us.npy').T
+    simulate(us, plot=True)
 
 
 
