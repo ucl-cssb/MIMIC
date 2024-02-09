4268271c1029b618db4d9dee5506d9cf78ba5bfb
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2024-02-09 12:04:56 +0000

    Added API documentation for most of the modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,15 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
 project = 'MIMIC'
 copyright = '2024, Pedro Fontanarrosa'
 author = 'Pedro Fontanarrosa'
 release = '0.1.0'
 
+# If extensions (or modules to document with autodoc) are in another
+# directory, add these directories to sys.path here. If the directory is
+# relative to the documentation root, use os.path.abspath to make it
+# absolute, like shown here.
+#
+sys.path.insert(0, os.path.abspath('../..'))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
