57dc9e601bd44e63e4e9d93a0f634ff59c5fba28
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2024-02-19 10:53:10 -0800

    Added __init__.py on root directory

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# Define package version
+__version__ = '0.1.0'
+
+# You can also include any global package initialization here
+
+# Define the __all__ variable
+__all__ = ['VARsim', 'gMLV_sim']
+# This will allow you to use the following import statement:
+# from mimic import VARsim, gMLV_sim
+# instead of:
+# from mimic.model_VAR.sim import VARsim
+# from mimic.gMLV.gMLV_sim import *
